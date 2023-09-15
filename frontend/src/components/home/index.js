8ea79c7940e22f1bc990ed9a60d2bf887761d8c8
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-09-15 20:06:17 +0630

    remove: Home component

--- frontend/src/components/home/index.js
+++ frontend/src/components/home/index.js
@@ -1,1 +1,24 @@
-export { default } from "./Home";
+import React, { useState } from "react";
+import MainSlider from "./MainSlider";
+import Banner from "./Banner";
+import ProductListings from "../common/ProductListings";
+import TopCollection from "./TopCollection";
+import Blogs from "./Blogs";
+import Brands from "./Brands";
+const Home = () => {
+  const [state, setState] = useState("");
+
+  return (
+    <>
+      <MainSlider />
+      <Banner />
+      <ProductListings title={"Big Savings"} />
+      <ProductListings title={"New Arrivals"} />
+      <TopCollection />
+      <ProductListings title={"Trending Offers"} />
+      <Blogs />
+      <Brands />
+    </>
+  );
+};
+export default Home;
