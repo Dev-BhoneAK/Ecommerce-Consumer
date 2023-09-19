bb8f0bc3ebdb26cbcae5ec96d391f98dfc76ad29
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-09-19 23:53:33 +0630

    fix: show error message

--- backend-api/api-v1/middleware/errorHandlerMiddleware.js
+++ backend-api/api-v1/middleware/errorHandlerMiddleware.js
@@ -1,6 +1,6 @@
 exports.errorHandler = (err, req, res, next) => {
     console.log(err);
-    res.status(500).json({ message: 'Internal Server Error' });
+    res.status(500).json({ message: err.message || 'Internal Server Error' });
 };
 
 exports.notFound = (req, res, next) => {
