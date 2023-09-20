af9feb013b9ea33a3709c8944c9ad20d06b5603f
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-09-20 23:59:03 +0630

    feat: add get all blogs end point

--- backend-api/api-v1/controllers/blogController.js
+++ backend-api/api-v1/controllers/blogController.js
@@ -4,6 +4,9 @@
 
 const blogService = require('../services/blogService');
 
+// @desc    Get All Blogs
+// @route   GET /api/v1/consumer/blogs
+// @access  Public
 exports.getAllBlogs = async (req, res, next) => {
     try {
         const blogs = await blogService.getAllBlogs();
