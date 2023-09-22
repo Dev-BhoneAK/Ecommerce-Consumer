6f7c0ccc7d9fa8fc4eb93a0f70923e60d7e17ef3
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-09-22 23:10:52 +0630

    fix: import name change

--- frontend/src/components/home/index.js
+++ frontend/src/components/home/index.js
@@ -1,24 +1,21 @@
-import React, { useState } from "react";
-import MainSlider from "./MainSlider";
-import Banner from "./Banner";
-import ProductListings from "../common/ProductListings";
-import TopCollection from "./TopCollection";
-import Blogs from "./Blogs";
-import Brands from "./Brands";
+import MainSlider from './MainSlider';
+import Banner from './Banner';
+import ProductListings from '../common/ProductListings';
+import TopCollection from './TopCollection';
+import Blogs from './BlogSlider';
+import Brands from './BrandSlider';
 const Home = () => {
-  const [state, setState] = useState("");
-
-  return (
-    <>
-      <MainSlider />
-      <Banner />
-      <ProductListings title={"Big Savings"} />
-      <ProductListings title={"New Arrivals"} />
-      <TopCollection />
-      <ProductListings title={"Trending Offers"} />
-      <Blogs />
-      <Brands />
-    </>
-  );
+    return (
+        <>
+            <MainSlider />
+            <Banner />
+            <ProductListings title={'Big Savings'} />
+            <ProductListings title={'New Arrivals'} />
+            <TopCollection />
+            <ProductListings title={'Trending Offers'} />
+            <Blogs />
+            <Brands />
+        </>
+    );
 };
 export default Home;
