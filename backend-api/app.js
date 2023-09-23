f824ff7d39dae1aded5bd0e98048b180e1847d49
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-09-23 23:16:54 +0630

    feat: add middleware'

--- backend-api/app.js
+++ backend-api/app.js
@@ -1,25 +1,25 @@
-require("dotenv").config();
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+require('dotenv').config();
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const { errorHandler } = require("./api-v1/middleware/errorHandlerMiddleware");
-const database = require("./api-v1/config/database");
-const consumerRouter = require("./api-v1/routes/consumer");
+const { errorHandler } = require('./api-v1/middleware/errorHandlerMiddleware');
+const database = require('./api-v1/config/database');
+const consumerRouter = require('./api-v1/routes/consumer');
 
 // connect to database
 database.connect();
 
 var app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-console.log("port: " + process.env.PORT);
-app.use("/api/v1/consumer", consumerRouter);
+app.use(express.static(path.join(__dirname, 'public')));
+console.log('port: ' + process.env.PORT);
+app.use('/api/v1/consumer', consumerRouter);
 
 // catch 404 and forward to error handler
 app.use(errorHandler);
