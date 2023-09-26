14594eadaa3bf01540907e4ee2364d05eebcf02f
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-09-26 23:56:42 +0630

    feat:add blog detail

---
+++ frontend/src/components/Blogs/Detail.js
@@ -0,0 +1,95 @@
+import React from 'react';
+
+const BlogDetail = () => {
+    return (
+        <section class="blog-detail-page section-b-space ratio2_3">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12 blog-detail">
+                        <img
+                            src="../assets/images/blogs/2.jpg"
+                            class="blog-image lazyload"
+                            alt="Blog Detail"
+                        />
+                        <h3>
+                            Also the leap into electronic typesetting, remaining
+                            essentially unchanged.
+                        </h3>
+                        <ul class="post-social">
+                            <li>25 January 2018</li>
+                            <li>Posted By : Admin Admin</li>
+                            <li>
+                                <ul className="social-icons">
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa-brands fa-facebook" />
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa-brands fa-x-twitter" />
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa-brands fa-vk" />
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa-solid fa-link" />
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <p>
+                            Fusce scelerisque augue a viverra semper. Etiam nisi
+                            nibh, vestibulum quis augue id, imperdiet fringilla
+                            dolor. Nulla sed nisl vel nisi cursus finibus.
+                            Vivamus ut augue nec justo viverra laoreet. Nunc
+                            efficitur, arcu ac cursus gravida, lorem elit
+                            commodo urna, id viverra libero tellus non ipsum.
+                            Fusce molestie ultrices nibh feugiat pretium. Donec
+                            pulvinar arcu metus, et dapibus odio condimentum id.
+                            Quisque malesuada mauris sit amet dui feugiat, ut
+                            pretium mauris luctus. Ut aliquam, tellus nec
+                            molestie condimentum, tellus arcu dignissim quam, a
+                            gravida nunc nulla vel magna. Sed pulvinar tortor et
+                            euismod blandit. Proin accumsan orci ac nunc
+                            fermentum vehicula.
+                        </p>
+                        <p>
+                            Cras quis neque urna. Pellentesque mollis, dui nec
+                            elementum elementum, ipsum quam suscipit ligula, sit
+                            amet lobortis dolor sem sed neque. Vivamus consequat
+                            est non sodales efficitur. Aliquam sodales eleifend
+                            sodales. Aliquam auctor ipsum quis nisl facilisis,
+                            at convallis mauris iaculis. Duis eleifend, magna ac
+                            convallis blandit, dui dui auctor leo, sed tincidunt
+                            nisi mauris ut nulla. Praesent porttitor dui ac
+                            turpis commodo porttitor. Integer ligula nisi,
+                            bibendum non sem at, porta condimentum dui.
+                        </p>
+                        <p>
+                            Proin id eleifend diam, euismod dictum nibh. Ut
+                            ullamcorper in purus at tempor. Nullam mattis risus
+                            nec velit semper lobortis. Donec accumsan ligula
+                            fermentum, ultricies massa eget, cursus leo.
+                            Suspendisse placerat elit et lacus aliquam, ut
+                            elementum leo aliquet. Integer ornare, ipsum eu
+                            lacinia viverra, lectus ipsum scelerisque nisl, nec
+                            iaculis leo elit id arcu. Aliquam id ante elit.
+                            Donec commodo purus eget lacus pharetra, et egestas
+                            metus blandit. Quisque pellentesque porta urna, sed
+                            dictum enim viverra a.
+                        </p>
+                    </div>
+                </div>
+                <div class="row section-b-space blog-advance"></div>
+            </div>
+        </section>
+    );
+};
+
+export default BlogDetail;
