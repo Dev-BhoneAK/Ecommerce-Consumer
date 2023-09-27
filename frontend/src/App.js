4daa56320a6df05c2707cbbe12a85663690abb2a
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-09-27 23:24:10 +0630

    feat: add Blog Detail route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,10 @@ import { Route, Routes } from 'react-router-dom';
 import Home from 'components/Home';
 import ProductDetail from 'components/ProductDetail';
 import Header from 'components/common/Header';
-import Footer from 'components/common/Footer/Footer';
-import Brands from 'components/Brands';
-import Blogs from 'components/Blogs';
+import Footer from 'components/common/Footer';
+import Brands from 'components/Brands/List';
+import Blogs from 'components/Blogs/List';
+import BlogDetail from 'components/Blogs/Detail';
 import PageNotFound from 'components/common/PageNotFound';
 import LoadingSkeleton from 'components/ProductDetail/LoadingSkeleton';
 
@@ -18,6 +19,7 @@ function App() {
                 <Route path="/product/:id" element={<ProductDetail />} />
                 <Route path="/brands" element={<Brands />} />
                 <Route path="/blogs" element={<Blogs />} />
+                <Route path="/blogs/:id" element={<BlogDetail />} />
                 <Route path="*" element={<PageNotFound />} />
                 {/*<Route path="/product/:id" element={<LoadingSkeleton />} />*/}
             </Routes>
