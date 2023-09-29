6350524b2269a796d4f325c72efce99cca5b53f5
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-09-29 23:51:35 +0630

    fix: change logo

--- frontend/src/components/common/Header/MainLogo.js
+++ frontend/src/components/common/Header/MainLogo.js
@@ -1,19 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const MainLogo = () => {
-  return (
-    <div className="menu-left">
-      <div className="brand-logo">
-        <Link to="/">
-          <img
-            src="/assets/icons/logo.png"
-            className="img-fluid lazyload"
-            alt="Website Logo"
-          />
-        </Link>
-      </div>
-    </div>
-  );
+    return (
+        <div className="menu-left">
+            <div className="brand-logo">
+                <Link to="/">
+                    <img
+                        src="/company-logo.png"
+                        className="img-fluid lazyload"
+                        alt="Company Logo"
+                    />
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export default MainLogo;
