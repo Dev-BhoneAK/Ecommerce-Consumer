4f97deabf726a87fddf0f2aef797175a1ea829b9
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-10-02 05:46:45 +0630

    feat: separate production and dev environment

--- backend-api/api-v1/config/database.js
+++ backend-api/api-v1/config/database.js
@@ -1,12 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 exports.connect = async () => {
-  try {
-    const connectionString =
-      process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/myshop"; // use default mongoDB port and database name if env not provided
-    await mongoose.connect(connectionString);
-    console.log("MongoDB connected!");
-  } catch (err) {
-    console.log(err);
-  }
+    try {
+        console.log('MONGODB_URI: ' + process.env.MONGODB_URI);
+
+        const connectionString =
+            process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/myshop'; // use default mongoDB port and database name if env not provided
+        await mongoose.connect(connectionString);
+
+        console.log('MongoDB connected!');
+    } catch (err) {
+        console.log(err);
+    }
 };
