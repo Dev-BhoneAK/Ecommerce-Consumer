9eff05274ee8e1958f0ba2099c3fbf7278987f3e
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-10-02 05:48:16 +0630

    feat: add api endpoints

--- frontend/src/config/API.js
+++ frontend/src/config/API.js
@@ -1,1 +1,10 @@
-export const API_URL = "http://localhost:5000/api/v1/consumer/";
+// export const API_URL = 'http://localhost:5000/api/v1/consumer/';
+// export const BASE_URL =
+//   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
+const BASE_URL = '/api/v1/consumer'; // If using proxy
+export const BRANDS_URL = BASE_URL + '/brands';
+export const BLOGS_URL = BASE_URL + '/blogs';
+export const PRODUCTS_URL = BASE_URL + '/products';
+// export const USERS_URL = '/api/users';
+// export const ORDERS_URL = '/api/orders';
+// export const PAYPAL_URL = '/api/config/paypal';
