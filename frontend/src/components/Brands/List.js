a0eab1c2bbf95efe89b808f629066bc6463be1c5
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-10-03 23:56:20 +0630

    feat: get api data for brand list

--- frontend/src/components/Brands/List.js
+++ frontend/src/components/Brands/List.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+
+import { BRANDS_URL } from 'config/API';
 
 const List = () => {
+    console.log('brands url ', BRANDS_URL);
+    const { data, isLoading, error } = useQuery(['brands'], () =>
+        axios.get(BRANDS_URL)
+    );
+    // const { isLoading, error, data } = useQuery(['repoData'], () =>
+    //     fetch('https://api.github.com/repos/tannerlinsley/react-query').then(
+    //         (res) => res.json()
+    //     )
+    // );
+
+    if (isLoading) return 'Loading...';
+
+    if (error) return 'An error has occurred: ' + error.message;
+
     return (
         <section class="brand-section brand-padding grid-brand ratio2_3">
             <div class="container">
                 <div class="row zoom-gallery">
-                    <div class="isotopeSelector filter fashion col-lg-3 col-sm-6">
+                    {data?.data.map((brand) => (
+                        <div class="isotopeSelector filter fashion col-lg-3 col-sm-6">
+                            <div class="overlay">
+                                <div class="border-brand">
+                                    {/* <a href="../assets/images/portfolio/grid/1.jpg"> */}
+                                    <div class="overlay-background"></div>
+                                    <img
+                                        src={`assets${brand.logo}`}
+                                        class="img-fluid lazyload bg-img"
+                                        alt="apple"
+                                    />
+                                    {/* </a> */}
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                    {/* <div class="isotopeSelector filter fashion col-lg-3 col-sm-6">
                         <div class="overlay">
                             <div class="border-brand">
                                 <a href="../assets/images/portfolio/grid/1.jpg">
@@ -32,7 +66,7 @@ const List = () => {
                                 </a>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </section>
