28f7a84d271f25dfc67ff5b80f6001936f34c85c
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-10-03 23:56:10 +0630

    feat: get api data for blog list

--- frontend/src/components/Blogs/List.js
+++ frontend/src/components/Blogs/List.js
@@ -1,20 +1,74 @@
 import React from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+
+import { BLOGS_URL } from 'config/API';
 
 const List = () => {
+    const { data, isLoading, error } = useQuery(['blogs'], () =>
+        axios.get(BLOGS_URL)
+    );
+
+    if (isLoading) return <LoadingSkeleton />;
     return (
-        <section class="section-b-space blog-page ratio2_3">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 col-lg-6 blog-media">
+        <section className="section-b-space blog-page ratio2_3">
+            <div className="container">
+                <div className="row">
+                    {data?.data.map((blog) => (
+                        <div className="col-12 col-lg-6 blog-media">
+                            <Link to={`/blogs/${blog.id}`}>
+                                <div className="row">
+                                    <img
+                                        src={`assets${blog.image}`}
+                                        className="blog-image lazyload bg-img"
+                                        alt="Blog"
+                                    />
+                                    <div className="blog-content">
+                                        <div>
+                                            <h6>25 January 2018</h6>{' '}
+                                            <h4>
+                                                you how all this mistaken idea
+                                                of denouncing pleasure and
+                                                praising pain was born.
+                                            </h4>
+                                            <ul className="post-social">
+                                                <li>Posted By : Admin Admin</li>
+                                                <li>
+                                                    <i className="fa fa-heart"></i>{' '}
+                                                    5 Hits
+                                                </li>
+                                                <li>
+                                                    <i className="fa fa-comments"></i>{' '}
+                                                    10 Comment
+                                                </li>
+                                            </ul>
+                                            <p>
+                                                Consequences that are extremely
+                                                painful. Nor again is there
+                                                anyone who loves or pursues or
+                                                desires to obtain pain of
+                                                itself, because it is pain, but
+                                                because occasionally
+                                                circumstances occur in which
+                                                toil and pain can procure him
+                                                some great pleasure.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Link>
+                        </div>
+                    ))}
+                    {/* <div className="col-12 col-lg-6 blog-media">
                         <Link to="/blogs/1">
-                            <div class="row">
+                            <div className="row">
                                 <img
                                     src="assets/images/blogs/1.jpg"
-                                    class="blog-image lazyload bg-img"
+                                    className="blog-image lazyload bg-img"
                                     alt="Blog"
                                 />
-                                <div class="blog-content">
+                                <div className="blog-content">
                                     <div>
                                         <h6>25 January 2018</h6>{' '}
                                         <h4>
@@ -22,14 +76,14 @@ const List = () => {
                                             denouncing pleasure and praising
                                             pain was born.
                                         </h4>
-                                        <ul class="post-social">
+                                        <ul className="post-social">
                                             <li>Posted By : Admin Admin</li>
                                             <li>
-                                                <i class="fa fa-heart"></i> 5
-                                                Hits
+                                                <i className="fa fa-heart"></i>{' '}
+                                                5 Hits
                                             </li>
                                             <li>
-                                                <i class="fa fa-comments"></i>{' '}
+                                                <i className="fa fa-comments"></i>{' '}
                                                 10 Comment
                                             </li>
                                         </ul>
@@ -48,15 +102,15 @@ const List = () => {
                             </div>
                         </Link>
                     </div>
-                    <div class="col-12 col-lg-6 blog-media">
+                    <div className="col-12 col-lg-6 blog-media">
                         <Link to="/blogs/2">
-                            <div class="row">
+                            <div className="row">
                                 <img
                                     src="assets/images/blogs/3.jpg"
-                                    class="blog-image lazyload bg-img"
+                                    className="blog-image lazyload bg-img"
                                     alt="Blog"
                                 />
-                                <div class="blog-content">
+                                <div className="blog-content">
                                     <div>
                                         <h6>25 January 2018</h6>{' '}
                                         <h4>
@@ -64,14 +118,14 @@ const List = () => {
                                             denouncing pleasure and praising
                                             pain was born.
                                         </h4>
-                                        <ul class="post-social">
+                                        <ul className="post-social">
                                             <li>Posted By : Admin Admin</li>
                                             <li>
-                                                <i class="fa fa-heart"></i> 5
-                                                Hits
+                                                <i className="fa fa-heart"></i>{' '}
+                                                5 Hits
                                             </li>
                                             <li>
-                                                <i class="fa fa-comments"></i>{' '}
+                                                <i className="fa fa-comments"></i>{' '}
                                                 10 Comment
                                             </li>
                                         </ul>
@@ -90,15 +144,15 @@ const List = () => {
                             </div>
                         </Link>
                     </div>
-                    <div class="col-12 col-lg-6 blog-media">
+                    <div className="col-12 col-lg-6 blog-media">
                         <Link to="/blogs/3">
-                            <div class="row">
+                            <div className="row">
                                 <img
                                     src="assets/images/blogs/3.jpg"
-                                    class="blog-image lazyload bg-img"
+                                    className="blog-image lazyload bg-img"
                                     alt="Blog"
                                 />
-                                <div class="blog-content">
+                                <div className="blog-content">
                                     <div>
                                         <h6>25 January 2018</h6>{' '}
                                         <h4>
@@ -106,14 +160,14 @@ const List = () => {
                                             denouncing pleasure and praising
                                             pain was born.
                                         </h4>
-                                        <ul class="post-social">
+                                        <ul className="post-social">
                                             <li>Posted By : Admin Admin</li>
                                             <li>
-                                                <i class="fa fa-heart"></i> 5
-                                                Hits
+                                                <i className="fa fa-heart"></i>{' '}
+                                                5 Hits
                                             </li>
                                             <li>
-                                                <i class="fa fa-comments"></i>{' '}
+                                                <i className="fa fa-comments"></i>{' '}
                                                 10 Comment
                                             </li>
                                         </ul>
@@ -131,11 +185,76 @@ const List = () => {
                                 </div>
                             </div>
                         </Link>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </section>
     );
 };
+
+const LoadingSkeleton = () => (
+    // <div className="loader_skeleton">
+    <section className="section-b-space blog-page ratio2_3">
+        <div className="container">
+            <div className="row">
+                <div className="col-xl-12 order-sec">
+                    <div className="product-box">
+                        <div className="media">
+                            <div className="img-wrapper"></div>
+                            <div className="media-body align-self-center">
+                                <div className="product-detail">
+                                    <h4></h4>
+                                    <h5></h5>
+                                    <h5 className="second"></h5>
+                                    <h6></h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="product-box">
+                        <div className="media">
+                            <div className="img-wrapper"></div>
+                            <div className="media-body align-self-center">
+                                <div className="product-detail">
+                                    <h4></h4>
+                                    <h5></h5>
+                                    <h5 className="second"></h5>
+                                    <h6></h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="product-box">
+                        <div className="media">
+                            <div className="img-wrapper"></div>
+                            <div className="media-body align-self-center">
+                                <div className="product-detail">
+                                    <h4></h4>
+                                    <h5></h5>
+                                    <h5 className="second"></h5>
+                                    <h6></h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="product-box">
+                        <div className="media">
+                            <div className="img-wrapper"></div>
+                            <div className="media-body align-self-center">
+                                <div className="product-detail">
+                                    <h4></h4>
+                                    <h5></h5>
+                                    <h5 className="second"></h5>
+                                    <h6></h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    // </div>
+);
 
 export default List;
