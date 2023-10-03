fcf12221d687f8bf0de554b96e88a534cba89187
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-10-03 23:55:41 +0630

    feat: add Section Title component

---
+++ frontend/src/components/home/SectionTitle.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SectionTitle = ({ title }) => {
+    return (
+        <div className="col-12">
+            <div className="title-basic">
+                <h2 className="title font-fraunces">
+                    <i class="fa-solid fa-bolt" />
+                    {title}
+                </h2>
+            </div>
+        </div>
+    );
+};
+
+export default SectionTitle;
