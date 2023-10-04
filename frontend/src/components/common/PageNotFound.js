662a0fce2ca03afc4475119df287cc584fdfefc7
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-10-04 23:33:18 +0630

    fix: change className

--- frontend/src/components/common/PageNotFound.js
+++ frontend/src/components/common/PageNotFound.js
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
   return (
-    <section class="p-0">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-12">
-            <div class="error-section">
+    <section className="p-0">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="error-section">
               <h1>404</h1>
               <h2>page not found</h2>
-              <Link to="/" class="btn btn-solid">
+              <Link to="/" className="btn btn-solid">
                 back to home
               </Link>
             </div>
