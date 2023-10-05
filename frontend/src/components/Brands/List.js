9073c0d5e173dd057fef7117155499a2d488d33c
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-10-05 23:33:44 +0630

    feat: get API data

--- frontend/src/components/Brands/List.js
+++ frontend/src/components/Brands/List.js
@@ -4,73 +4,44 @@ import { useQuery } from '@tanstack/react-query';
 
 import { BRANDS_URL } from 'config/API';
 
-const List = () => {
+const BrandList = () => {
     console.log('brands url ', BRANDS_URL);
     const { data, isLoading, error } = useQuery(['brands'], () =>
         axios.get(BRANDS_URL)
     );
-    // const { isLoading, error, data } = useQuery(['repoData'], () =>
-    //     fetch('https://api.github.com/repos/tannerlinsley/react-query').then(
-    //         (res) => res.json()
-    //     )
-    // );
 
     if (isLoading) return 'Loading...';
 
-    if (error) return 'An error has occurred: ' + error.message;
+    if (error) return 'An error has occurred: ' + error;
 
     return (
-        <section class="brand-section brand-padding grid-brand ratio2_3">
-            <div class="container">
-                <div class="row zoom-gallery">
+        <section className="brand-section brand-padding grid-brand ratio2_3">
+            <div className="container">
+                <div className="row zoom-gallery">
                     {data?.data.map((brand) => (
-                        <div class="isotopeSelector filter fashion col-lg-3 col-sm-6">
-                            <div class="overlay">
-                                <div class="border-brand">
-                                    {/* <a href="../assets/images/portfolio/grid/1.jpg"> */}
-                                    <div class="overlay-background"></div>
-                                    <img
-                                        src={`assets${brand.logo}`}
-                                        class="img-fluid lazyload bg-img"
-                                        alt="apple"
-                                    />
-                                    {/* </a> */}
-                                </div>
-                            </div>
-                        </div>
+                        <BrandCard brandData={brand} key={brand._id} />
                     ))}
-                    {/* <div class="isotopeSelector filter fashion col-lg-3 col-sm-6">
-                        <div class="overlay">
-                            <div class="border-brand">
-                                <a href="../assets/images/portfolio/grid/1.jpg">
-                                    <div class="overlay-background"></div>
-                                    <img
-                                        src="assets/images/brands/jbl.png"
-                                        class="img-fluid lazyload bg-img"
-                                        alt="apple"
-                                    />
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="isotopeSelector filter fashion col-lg-3 col-sm-6">
-                        <div class="overlay">
-                            <div class="border-brand">
-                                <a href="../assets/images/portfolio/grid/1.jpg">
-                                    <div class="overlay-background"></div>
-                                    <img
-                                        src="assets/images/brands/casestudi.png"
-                                        class="img-fluid lazyload bg-img"
-                                        alt="apple"
-                                    />
-                                </a>
-                            </div>
-                        </div>
-                    </div> */}
                 </div>
             </div>
         </section>
     );
 };
 
-export default List;
+const BrandCard = ({ brandData }) => (
+    <div className="isotopeSelector filter fashion col-lg-3 col-sm-6">
+        <div className="overlay">
+            <div className="border-brand">
+                {/* <a href="../assets/images/portfolio/grid/1.jpg"> */}
+                <div className="overlay-background"></div>
+                <img
+                    src={`assets${brandData.logo}`}
+                    className="img-fluid lazyload bg-img"
+                    alt={brandData.name}
+                />
+                {/* </a> */}
+            </div>
+        </div>
+    </div>
+);
+
+export default BrandList;
