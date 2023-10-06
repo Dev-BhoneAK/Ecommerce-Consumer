bc99bfe6f5eded25ecbf5882948c1aea5429a21a
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-10-06 23:50:37 +0630

    feat: add section container on Product Detail

--- frontend/src/components/ProductDetail/index.js
+++ frontend/src/components/ProductDetail/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import Summary from "./Summary";
-import AdditionalInformation from "./AdditionalInformation";
-import ProductListings from "../common/ProductListings";
+import React from 'react';
+import Summary from './Summary';
+import AdditionalInformation from './AdditionalInformation';
+import ProductListings from '../common/ProductListings';
 const ProductDetail = () => {
-  return (
-    <>
-      <Summary />
-      <AdditionalInformation />
-      <ProductListings title={"Related Products"} />
-    </>
-  );
+    return (
+        <section className="section-b-space">
+            <Summary />
+            <AdditionalInformation />
+            <ProductListings title={'Related Products'} />
+        </section>
+    );
 };
 export default ProductDetail;
