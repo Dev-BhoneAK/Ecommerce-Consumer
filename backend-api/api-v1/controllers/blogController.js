c4754b16323b04fcc35f81224b2746cd86ea7180
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-10-07 23:56:51 +0630

    feat: add endpoint for requested blog

--- backend-api/api-v1/controllers/blogController.js
+++ backend-api/api-v1/controllers/blogController.js
@@ -15,3 +15,16 @@ exports.getAllBlogs = async (req, res, next) => {
         next(err);
     }
 };
+
+// @desc    Get Requested Blog
+// @route   GET /api/v1/consumer/blogs/:blogId
+// @access  Public
+exports.getBlog = async (req, res, next) => {
+    try {
+        const blogId = req.params.blogId;
+        const blog = await blogService.getBlog(blogId);
+        res.status(200).json(blog);
+    } catch (err) {
+        next(err);
+    }
+};
