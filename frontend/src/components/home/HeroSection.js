b77a93756c71f50721dc780feca48711397ab4bf
Bhone Aung Kyaw <bhoneak@outlook.com>
2023-10-11 23:52:34 +0630

    feat: add Hero Section

---
+++ frontend/src/components/home/HeroSection.js
@@ -0,0 +1,13 @@
+import MainSlider from './MainSlider';
+import Banner from './Banner';
+
+function HeroSection() {
+    return (
+        <>
+            <MainSlider />
+            <Banner />
+        </>
+    );
+}
+
+export default HeroSection;
